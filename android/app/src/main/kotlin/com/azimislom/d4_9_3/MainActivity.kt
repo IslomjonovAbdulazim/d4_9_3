205ca31d08f574290428cccce6ed90f2075c46c5
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-05-07 18:37:03 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/azimislom/d4_9_3/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.azimislom.d4_9_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
+
+class ShowcaseWidget extends StatelessWidget {
+  const ShowcaseWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
